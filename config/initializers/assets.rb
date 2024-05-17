11869078d5eea261fdf161b57dcfcd6489ad2098
ytaros <hoo.miii0616@gmail.com>
2024-05-17 21:37:01 +0900

    FIX: esbuildの設定、ページの確認

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,7 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# config/initializers/assets.rb
+Rails.application.config.assets.paths << Rails.root.join('app/assets/builds')
+Rails.application.config.assets.precompile += %w(application.js application.css)
+
