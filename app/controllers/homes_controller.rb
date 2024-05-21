642162da13a94947421492db253a1e3c562a07ac
ytaros <hoo.miii0616@gmail.com>
2024-05-21 23:59:27 +0900

    fix: Policyの変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class HomesController < ApplicationController
+  before_action :home_authorize
+
   def index; end
+
+  private
+
+  def home_authorize
+    authorize :home
+  end
 end
