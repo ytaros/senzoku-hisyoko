87464d78c1335b8d941f488ad12f2c4cb83c606b
ytaros <hoo.miii0616@gmail.com>
2024-05-25 00:22:04 +0900

    fix: ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get    '/admin_login',   to: 'admin_sessions#new'
   post   '/admin_login',   to: 'admin_sessions#create'
   delete '/admin_logout',  to: 'admin_sessions#destroy'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
 
   resources :tenants
+  resources :users
 end
