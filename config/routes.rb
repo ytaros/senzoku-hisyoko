fff0830e93e98944add5239db679abc71b1ff9bd
ytaros <hoo.miii0616@gmail.com>
2024-05-25 16:50:11 +0900

    fix: 辞書ファイル、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   delete '/admin_logout',  to: 'admin_sessions#destroy'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :tenants
   resources :users
