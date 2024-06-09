beef47199752b42d6329af95f2fa25149e357220
ytaros <hoo.miii0616@gmail.com>
2024-06-09 23:31:25 +0900

    fix: デプロイ対応

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,5 +7,5 @@ yarn install
 yarn build
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate:reset
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create db:migrate
 bundle exec rails db:seed
