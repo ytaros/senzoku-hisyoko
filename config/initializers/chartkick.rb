4b8212048aac44037957be2faf8d5987c1cedfea
ytaros <hoo.miii0616@gmail.com>
2024-06-12 22:06:49 +0900

    add: グラフのカラーなど設定

---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,9 @@
+Chartkick.options = {
+  height: "300px",
+  colors: [
+    "#FFB3BA", "#FFDFBA", "#FFFFBA", "#BAFFC9", "#BAE1FF",
+    "#FFB3FF", "#FFBABA", "#D4FFB2", "#FFC4BA", "#BAFFFF",
+    "#FFD1DC", "#FFE4E1", "#FFF0F5", "#F0FFF0", "#F0FFFF",
+    "#F5F5DC", "#FFFACD", "#E0FFFF", "#FAFAD2", "#D3FFCE"
+  ]
+}
