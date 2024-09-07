371387990508cfda53db196308c6741f9a3ccf2f
ytaros <hoo.miii0616@gmail.com>
2024-09-07 19:27:17 +0900

    本番用アカウントを作成するため環境変数でseedファイルを設定

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,5 @@ yarn install
 yarn build
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+bundle exec rails db:migrate:reset
+bundle exec rails db:seed RAILS_ENV=production
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Admin.create(
-  name: 'admin',
-  login_id: 'ytaros',
-  password: 'password',
-  password_confirmation: 'password'
+  name: ENV['ADMIN_NAME'],
+  login_id: ENV['ADMIN_LOGIN_ID'],
+  password: ENV['ADMIN_PASSWORD'],
+  password_confirmation: ENV['ADMIN_PASSWORD']
 )
