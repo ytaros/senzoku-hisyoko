fc5ee36c4c3755a9162162090a408339a1b475f8
ytaros <hoo.miii0616@gmail.com>
2024-09-07 19:00:28 +0900

    fix: デプロイ設定（本番環境のDBを削除）

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,4 @@ yarn install
 yarn build
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate:reset
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
