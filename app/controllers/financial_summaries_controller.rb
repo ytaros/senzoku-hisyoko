d00c2b1661e0d596072a5786134af853b7576d97
ytaros <hoo.miii0616@gmail.com>
2024-09-12 14:22:32 +0900

    fix: current_userのレコードのみコンパイル、表示

--- app/controllers/financial_summaries_controller.rb
+++ app/controllers/financial_summaries_controller.rb
@@ -5,15 +5,15 @@ class FinancialSummariesController < ApplicationController
 
   def index
     @month = parse_month(params[:month]) || Date.today.beginning_of_month
-    @summaries = FinancialSummaryService.summarize(@month)
-    @monthly_summary = FinancialSummaryService.monthly_summary(@month)
+    @summaries = FinancialSummaryService.summarize(@month, current_user)
+    @monthly_summary = FinancialSummaryService.monthly_summary(@month, current_user)
     @formatted_food_for_month = OrderDetail.format_data_for_period(:food_for_month, @month)
     @formatted_drink_for_month = OrderDetail.format_data_for_period(:drink_for_month, @month)
   end
 
   def show
     @date = Date.parse(params[:date])
-    @daily_summary = FinancialSummaryService.daily_summary(@date)
+    @daily_summary = FinancialSummaryService.daily_summary(@date, current_user)
     @formatted_food_for_day = OrderDetail.format_data_for_period(:food_for_day, @date)
     @formatted_drink_for_day = OrderDetail.format_data_for_period(:drink_for_day, @date)
   end
@@ -21,7 +21,7 @@ class FinancialSummariesController < ApplicationController
   def compile
     begin
       date = Date.parse(params[:compile][:date])
-      if FinancialSummaryService.compile_for_date(date)
+      if FinancialSummaryService.compile_for_date(date, current_user)
         flash[:success] = t('.success', summary_date: date.strftime('%Y年%m月%d日'))
       else
         flash[:danger] = t('.records_not_found', summary_date: date.strftime('%Y年%m月%d日'))
